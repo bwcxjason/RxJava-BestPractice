34563488b85541201d461e266c8243a7609560ab
bwcxjason <342430895@qq.com>
2018-06-13 21:33:10 +0800

    Observable Methods.

--- app/src/main/java/com/example/jason/rxjava_bestpractice/gettingstarted/HelloWorld.java
+++ app/src/main/java/com/example/jason/rxjava_bestpractice/gettingstarted/HelloWorld.java
@@ -1,11 +1,21 @@
 package com.example.jason.rxjava_bestpractice.gettingstarted;
 
 
+import android.annotation.SuppressLint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class HelloWorld {
 
@@ -58,10 +68,327 @@ public class HelloWorld {
         return observer;
     }
 
+    private static void create() {
+        Observable.just("1", "2", "3", "Hello", "World").subscribe(new Observer<String>() {
+            Disposable disposable;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable = d;
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                if ("3".equals(s)) {
+                    disposable.dispose();
+                    return;
+                }
+                System.out.println("onNext:" + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError:" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
+    }
+
+    @SuppressLint("CheckResult")
+    private static void fromCallable() {
+        Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Hello world!";
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println(s);
+            }
+        });
+    }
+
+    @SuppressLint("CheckResult")
+    private static void fromFuture() {
+        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "Future World!";
+            }
+        });
+
+        Observable.fromFuture(futureTask).doOnSubscribe(new Consumer<Disposable>() {
+            @Override
+            public void accept(Disposable disposable) throws Exception {
+                futureTask.run();
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println(s);
+            }
+        });
+    }
+
+    @SuppressLint("CheckResult")
+    private static void fromIterable() {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Observable
+                .fromIterable(list)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("onNext:" + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static Integer i;
+    private static void defer() {
+        i = 100;
+        Observable<Integer> observable = Observable.defer(new Callable<ObservableSource<? extends Integer>>() {
+            @Override
+            public ObservableSource<? extends Integer> call() throws Exception {
+                return Observable.just(i);
+            }
+        });
+
+        i = 200;
+
+        Observer observer = new Observer<Integer>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext:" + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError:" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        observable.subscribe(observer);
+
+        i = 300;
+
+        observable.subscribe(observer);
+    }
+
+    private static void timer() {
+        Observable
+                .timer(2, TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        System.out.println("onNext:" + aLong);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError:" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static void interval() {
+        Observable
+                .interval(4, TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        System.out.println("onNext:" + aLong);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError:" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static void range() {
+        Observable
+                .range(2, 15)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("onNext:" + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError:" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static void empty() {
+        Observable
+                .empty()
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        System.out.println("onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static void never() {
+        Observable
+                .never()
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        System.out.println("onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+    private static void error() {
+        Observable
+                .error(new NullPointerException())
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        System.out.println("onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
     public static void main(String[] args) {
         Observable observable = createObservable();
         Observer observer = createObserver();
         observable.subscribe(observer);
+
+        // or
+        create();
+
+        fromCallable();
+        fromFuture();
+        fromIterable();
+
+        defer();
+
+        timer();
+        interval();
+
+        range();
+
+        empty();
+        never();
+        error();
     }
 
 }
